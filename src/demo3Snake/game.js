99c8ba4e0db2be4c0d3b64719bd93b0d373eee52
gauravha <47674146+gauravha@users.noreply.github.com>
2019-05-04 21:13:08 -0400

    Update game.js

--- src/demo3Snake/game.js
+++ src/demo3Snake/game.js
@@ -1,41 +1,46 @@
 
 function Game (game){
-  this.snakes = []
+    this.snakes = []
 }
 
 Game.prototype = {
-  function preload(){this.game.load.image('circle','src/assets/circle.png');
-   this.game.load.image('background', 'src/assets/tile.png');}
+    preload: function() {
+        this.game.load.image('circle','asset/circle.png');
+
+    this.game.load.image('background', 'asset/tile.png');
+},
 
 
-function create(){
-  //we're accessing the parameter game from the Game function
-  // var width = this.game.width;
-  // var height = this.game.height;
+    create: function() {
+    //we're accessing the parameter game from the Game function
+    // var width = this.game.width;
+    // var height = this.game.height;
 
-  this.game.world.setBounds(-this.game.width, -this.game.height, this.game.width*2, this.game.height*2);
- this.game.stage.backgroundColor = '#444';
+    this.game.world.setBounds(-this.game.width, -this.game.height, this.game.width * 2, this.game.height * 2);
+    this.game.stage.backgroundColor = '#444';
 
-  //add tilesprite background
-  //we're repeating tiles again and again to make a background. NOTE: Remove the background variable if not needed.
-  var background = this.game.add.tileSprite(-this.game.width, -this.game.height,
-      this.game.world.width, this.game.world.height, 'background');
+    //add tilesprite background
+    //we're repeating tiles again and again to make a background. NOTE: Remove the background variable if not needed.
+    var background = this.game.add.tileSprite(-this.game.width, -this.game.height,
+        this.game.world.width, this.game.world.height, 'background');
 
-  //initialize physics and groups
-  this.game.physics.startSystem(Phaser.Physics.P2JS);
+    //initialize physics and groups
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
 
 //NOTE: we might remove this line because we're already using it in the Game function
-  this.game.snakes = [];
+    this.game.snakes = [];
 
-  //create player
-  var snake = new Snake(this.game, 'circle', 0, 0);
-  //this line is required because it's referring to snake variable above, which is a new a player.
-  this.game.camera.follow(snake.head);
-}
-function update() {
+    //create player
+    var snake = new Snake(this.game, 'circle', 0, 0);
+    //this line is required because it's referring to snake variable above, which is a new a player.
+    this.game.camera.follow(snake.head);
+},
+
+    update: function() {
     //update game components
-    for (var i = this.game.snakes.length - 1 ; i >= 0 ; i--) {
+    for (var i = this.game.snakes.length - 1; i >= 0; i--) {
         this.game.snakes[i].update();
     }
 }
+
 }
