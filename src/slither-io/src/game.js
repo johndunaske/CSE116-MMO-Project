2b0ac17e7095cb54831fcca3fa58c3dcad2808ea
John K Dunaske <j_dunaske@yahoo.com>
2019-05-08 18:31:41 -0400

    Revert "Fixed an issue, networking works"
    This reverts commit 45e15ae946da4173ea9150f121e5b0045711f2cf.

--- src/slither-io/src/game.js
+++ src/slither-io/src/game.js
@@ -1,14 +1,6 @@
-Game = {}
+Game = function(game) {}
 
-Game = {
-    init: function() {
-
-        this.game.stage.disableVisibilityChange = true;
-
-        this.game.updateX = 0;
-        this.game.updateY = 0;
-    },
-
+Game.prototype = {
     preload: function() {
 
         //load assets
@@ -46,26 +38,16 @@ Game = {
         this.game.snakes = [];
 
         //create player
-        var randX = Math.floor(Math.random() * this.game.width);
-        var randY = Math.floor(Math.random() * this.game.width);
-
-        // var newPlayer = {
-        //     id: 0,
-        //     snake: new PlayerSnake(this.game, 'circle', randX, randY, )
-        // };
-        // this.game.snakes.push(newPlayer)
-        // this.game.camera.follow(newPlayer.snake.head);
-        Client.askNewPlayer(randX, randY);
+        var snake = new PlayerSnake(this.game, 'circle', 0, 0);
+        this.game.camera.follow(snake.head);
 
         //initialize snake groups and collision
-
         for (var i = 0 ; i < this.game.snakes.length ; i++) {
             var snake = this.game.snakes[i];
-            // snake["snake"].head.body.setCollisionGroup(this.snakeHeadCollisionGroup);
-            // snake["snake"].head.body.collides([this.foodCollisionGroup]);
-            // //callback for when a snake is destroyed
-            // snake.snake.addDestroyedCallback(this.snakeDestroyed, this);
-            console.log("bo")
+            snake.head.body.setCollisionGroup(this.snakeHeadCollisionGroup);
+            snake.head.body.collides([this.foodCollisionGroup]);
+            //callback for when a snake is destroyed
+            snake.addDestroyedCallback(this.snakeDestroyed, this);
         }
     },
     /**
@@ -73,13 +55,8 @@ Game = {
      */
     update: function() {
         //update game components
-        //console.log(this.game.snakes)
         for (var i = this.game.snakes.length - 1 ; i >= 0 ; i--) {
-            //console.log(this.game.snakes[i])
-            if (this.game.snakes[i] != null) {
-                this.game.snakes[i]["snake"].update(this.game.updateX, this.game.updateY);
-            }
-
+            this.game.snakes[i].update();
         }
         for (var i = this.foodGroup.children.length - 1 ; i >= 0 ; i--) {
             var f = this.foodGroup.children[i];
@@ -101,8 +78,6 @@ Game = {
     },
     snakeDestroyed: function(snake) {
         //place food where snake was destroyed
-        console.log("id: " + snake.id);
-        Client.playerDestroyed(snake);
         for (var i = 0 ; i < snake.headPath.length ;
         i += Math.round(snake.headPath.length / snake.snakeLength) * 2) {
             this.initFood(
@@ -110,51 +85,5 @@ Game = {
                 snake.headPath[i].y + Util.randomInt(-10,10)
             );
         }
-    },
-
-    addNewPlayer: function (id,x,y) {
-        console.log("adding new pLyaer")
-        var newPlayer = {
-            id: id,
-            snake: new PlayerSnake(this.game, "circle", x, y, id)
-        };
-        this.game.camera.follow(newPlayer.snake.head);
-        this.game.snakes.push(newPlayer);
-        newPlayer.snake.head.body.setCollisionGroup(this.snakeHeadCollisionGroup);
-        newPlayer.snake.head.body.collides([this.foodCollisionGroup]);
-        //callback for when a snake is destroyed
-        newPlayer.snake.addDestroyedCallback(this.snakeDestroyed, this);
-    },
-
-    addNewOpponent: function(id, x, y) {
-        var newPlayer = {
-            id: id,
-            snake: new OtherSnake(this.game, "circle", x, y, id)
-        };
-        this.game.snakes.push(newPlayer);
-        newPlayer.snake.head.body.setCollisionGroup(this.snakeHeadCollisionGroup);
-        newPlayer.snake.head.body.collides([this.foodCollisionGroup]);
-        //callback for when a snake is destroyed
-        newPlayer.snake.addDestroyedCallback(this.snakeDestroyed, this);
-    },
-    
-    removePlayer: function (data) {
-        for (var i = 0; i < this.game.snakes.length - 1; i++) {
-            if (this.game.snakes[i].id == data.id) {
-                delete this.game.snakes[i];
-            }
-        }
-    },
-
-    disconnectPlayer: function (id) {
-        for (var i = 0; i < this.game.snakes.length - 1; i++) {
-            if (this.game.snakes[i]["snake"].id == id) {
-                delete this.game.snakes[i];
-            }
-        }
-    },
-    setXY: function (x,y) {
-        this.game.updateX = x;
-        this.game.updateY = y;
     }
 };
